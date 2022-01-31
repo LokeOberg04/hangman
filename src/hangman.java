ccbaf9f222aa6ebd8c64e80f2a2de7af9448342e
pppog <77719393+pppog@users.noreply.github.com>
2022-01-31 11:20:37 +0100

    Finished Product

--- src/hangman.java
+++ src/hangman.java
@@ -9,44 +9,52 @@ public class hangman {
         char letterguess = 0;
         String wordguess = "";
         int lives = 3;
+        int correct = 0;
         char[] letters = new char[word.length()];
 
-        for(int i = 0; i < word.length(); i++) {
+        for (int i = 0; i < word.length(); i++) {
             letters[i] = word.charAt(i);
             if (letters[i] == ' ') {
-                underscore += "";
-            }
-                else if (letters[i] == '-') {
+                underscore += " ";
+            } else if (letters[i] == '-') {
                 underscore += "-";
-            }
-                else {
+            } else {
                 underscore += "_";
             }
         }
         char[] underscorelist = new char[underscore.length()];
-        System.out.println(underscorelist);
-        guess = JOptionPane.showInputDialog("Guess?");
-        if(guess.length()>1) {
-            wordguess = guess;
-            if(wordguess.equals(word)) {
-                System.out.println("you win!\n The word was " + word);
-            }
-            else {
-                System.out.println("you lose!\n The word was " + word + "\nYour guess was " + wordguess);
-            }
+        for (int j = 0; j < underscore.length(); j++) {
+            underscorelist[j] = underscore.charAt(j);
         }
-        else {
-            letterguess = guess.charAt(0);
-            for (int l = 0; l < word.length(); l++) {
-                if (letterguess == letters[l]) {
-                    letters[l] = letterguess;
-                    System.out.println(letters);
+        while (lives > 0) {
+            System.out.println(underscorelist);
+            guess = JOptionPane.showInputDialog("Guess?");
+            if (guess.length() > 1) {
+                wordguess = guess;
+                if (wordguess.equals(word)) {
+                    System.out.println("you win!\n The word was " + "'" + word + "'");
+                    System.exit(0);
+                } else {
+                    System.out.println("you lose!\n The word was '" + word + "'\nYour guess was " + "'" + wordguess + "'");
+                    System.exit(0);
                 }
-                else {
-                    lives = lives-1;
-                    System.out.println("Your letter was wrong\n" + "You have " + lives + " left");
+            } else {
+                letterguess = guess.charAt(0);
+                for (int l = 0; l < word.length(); l++) {
+                    if (letterguess == letters[l]) {
+                        underscorelist[l] = letterguess;
+                        correct = 1;
+                    } else {
+                    }
                 }
-        }
+                if (correct == 0) {
+                    lives = lives - 1;
+                    System.out.println("Your letter was wrong\n" + "You have " + lives + " lives left");
+                } else {
+                    correct = 0;
+                }
+            }
         }
     }
-}
+    }
+
